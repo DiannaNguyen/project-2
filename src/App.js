f13778a3b97abcb3f9986d8ba2ea5ca70d171532
DiannaNguyen <diannavnguyen@gmail.com>
2021-03-11 00:53:02 -0600

    Installed react router and set-up all the components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
-const key = process.env.REACT_APP_API_KEY;
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+
+
+// const key = process.env.REACT_APP_API_KEY;
+
+
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <nav>
+        <Link to="/">
+          <h1>Tell Me Something</h1>
+        </Link>
+      </nav>
+      <main>
+        <Route path="/" exact component={Home}/>
+      </main>
     </div>
   );
 }
