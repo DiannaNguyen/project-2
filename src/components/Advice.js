e3307e27de713382f0daaf76f1e63d589dbc5e26
DiannaNguyen <diannavnguyen@gmail.com>
2021-03-15 13:42:53 -0500

    Edit format

--- src/components/Advice.js
+++ src/components/Advice.js
@@ -2,36 +2,37 @@ import React, { useState, useEffect } from 'react';
 import '../App.css';
 
 const Advice = () => {
-    const [quote, setQuote] = useState(" ");
-    const [author, setAuthor] = useState(" ");
-    
+	const [quote, setQuote] = useState(' ');
+	const [author, setAuthor] = useState(' ');
+
 	useEffect(() => {
-		fetch("https:api.quotable.io/random")
-            .then((res) => res.json())
-            .then((quote) => {
-                setQuote(`${quote.content}`);
-                setAuthor(`-${quote.author}`);
-            })
-            .catch(console.error);
+		fetch('https:api.quotable.io/random')
+			.then((res) => res.json())
+			.then((data) => {
+				setQuote(`${data.content}`);
+				setAuthor(`-${data.author}`);
+			})
+			.catch(console.error);
 	}, []);
 
-       let getNewQuote = () => {
-					fetch("https:api.quotable.io/random")
-						.then((res) => res.json())
-						.then((quote) => {
-							setQuote(`${quote.content}`);
-							setAuthor(`-${quote.author}`);
-						});
-				};
+	let getNewQuote = () => {
+		fetch('https:api.quotable.io/random')
+			.then((res) => res.json())
+			.then((data) => {
+				setQuote(`${data.content}`);
+				setAuthor(`-${data.author}`);
+			});
+	};
 
-    return (
-			<div className="quote">
-				<h1>{quote}</h1>
-				<h2>{author}</h2>
-				<button className='button' onClick={getNewQuote}>Try Again, Not Feeling It
-				</button>
-			</div>
-		);
+	return (
+		<div className='quote'>
+			<h1>{quote}</h1>
+			<h2>{author}</h2>
+			<button className='button' onClick={getNewQuote}>
+				Try Again, Not Feeling It
+			</button>
+		</div>
+	);
 };
 
 export default Advice;
